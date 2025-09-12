244ea41ceb6f559a10135fa1957b45de61582bc0
gdm <god_mao@foxmail.com>
2025-09-13 01:36:39 +0800

    Site updated: 2025-09-13 01:36:36

---
+++ js/page_line.js
@@ -0,0 +1,7 @@
+document.addEventListener("scroll", () => {
+    const progressBar = document.getElementById("reading-progress");
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (scrollTop / scrollHeight) * 100;
+    progressBar.style.width = scrolled + "%";
+});
